57d029f72c2a239158aadba81eaaa886cc3e29d6
Sandeep <sandeepnandanwar92@gmail.com>
2022-03-22 02:32:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require("cors");
+
+//var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var bookingRouter = require('./routes/booking')
+var movie = require('./routes/create_moovie');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const corsOptions = {
+  origin: true, //included origin as true
+  credentials: true, //included credentials as true
+};
+app.use(cors(corsOptions));
+
+//app.use('/', indexRouter);
+app.use('/', usersRouter);
+app.use('/', bookingRouter);
+app.use('/',movie)
+
+if (process.env.Mode_Env = "production") {
+  app.use(express.static("client/build"));
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+
+}
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header('Access-Control-Allow-Credentials', false);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next()
+})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ client/src/components/Logout.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router'
+
+
+const Logout = (props) => {
+    const { dispatch } = props
+    const history = useHistory();
+    useEffect(() => {
+        axios.create({
+            //baseURL: "http://localhost:5000",
+            withCredentials: true,
+            credentials: "include",
+        }).get('/logout').then((res) => {
+            history.push('/login', { replace: true })
+            dispatch({ type: 'USER', payload: false })
+
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    return (
+        <div className='rightbar'>
+            <h1>Logout page</h1>
+        </div>
+    )
+}
+
+export default Logout
